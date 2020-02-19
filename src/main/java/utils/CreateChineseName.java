f2dbb52a684390e4a2e26a114e18cc160825ef47
D_h <54danghao@gmail.com>
2020-02-19 22:36:54 +0800

    指定姓氏随机生成姓名

--- src/main/java/utils/CreateChineseName.java
+++ src/main/java/utils/CreateChineseName.java
@@ -2,8 +2,6 @@ package utils;
 
 import constants.Constants;
 
-import java.util.concurrent.CompletionService;
-
 import static utils.RandomNum.getRandomNum;
 
 public class CreateChineseName {
@@ -25,15 +23,20 @@ public class CreateChineseName {
 		String firstName = Constants.BOY;
 		int firstNameCount = Constants.BOY.length();
 
-		// 性别
-		if (sex == 0) {
-			firstName = Constants.GIRL;
-			firstNameCount = Constants.GIRL.length();
-			gender = "女";
-		} else {
-			gender = "男";
-		}
+		return getName(lastName, firstName, firstNameCount, sex);
+	}
 
+	/**
+	 * <p>生成名字</p>
+	 *
+	 * @param lastName       姓
+	 * @param firstName      名1
+	 * @param firstNameCount 名2
+	 * @param gender         性别
+	 * @return 姓名
+	 */
+	private static String getName(String lastName, String firstName, int firstNameCount, int gender) {
+		int index;
 		index = getRandomNum(0, firstNameCount - 1);
 		String second = firstName.substring(index, index + 1);
 		int hasThird = getRandomNum(0, 1);
@@ -71,14 +74,26 @@ public class CreateChineseName {
 			gender = "男";
 		}
 
-		index = getRandomNum(0, firstNameCount - 1);
-		String second = firstName.substring(index, index + 1);
-		int hasThird = getRandomNum(0, 1);
-		String third = "";
-		if (hasThird == 1) {
-			index = getRandomNum(0, firstNameCount - 1);
-			third = firstName.substring(index, index + 1);
-		}
-		return lastName + second + third;
+		return getName(lastName, firstName, firstNameCount, firstNameCount);
 	}
+
+	/**
+	 * <p>指定姓氏随机生成姓名</p>
+	 * @param lastName 姓氏
+	 * @return 姓名
+	 */
+	public static String makeChineseName(String lastName) {
+
+		int index = getRandomNum(0, Constants.LASTNAME.length() - 1);
+
+		// 名
+		String firstName = Constants.BOY;
+		int firstNameCount = Constants.BOY.length();
+
+		// 性别
+		String gender;
+		int sex = getRandomNum(0, 1);
+		return getName(lastName, firstName, firstNameCount, sex);
+	}
+
 }
--- src/test/java/TestMethod.java
+++ src/test/java/TestMethod.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 
-import static services.MakeMockPersonInfo.makeTel;
+//import static services.MakeMockPersonInfo.makeTel;
 import static utils.CreateChineseName.makeChineseName;
 import static utils.CreateEmail.makeEmail;
 import static utils.CreateIDNum.makeIDNum;
@@ -9,37 +9,39 @@ import static utils.CreateIDNum.makeIDNum;
 class TestMethod {
 
 	@Test
-	void testName(){
+	void testName() {
 		for (int i = 0; i < 10; i++) {
 //		System.out.print(makeChineseName() + " ");
-		System.out.print(makeChineseName(false) + " ");
+			System.out.print(makeChineseName() + " ");
+			System.out.print('|');
+			System.out.print(makeChineseName(true) + " ");
+			System.out.print('|');
+			System.out.print(makeChineseName("党") + " ");
 
 		}
 	}
 
 	@Test
-	void testEmail(){
+	void testEmail() {
 		for (int i = 0; i < 10; i++) {
 //			System.out.print(makeEmail(7) + " ");
 //			System.out.println(makeEmail());
 			System.out.println(makeEmail("54danghao"));
 		}
 	}
-	@Test
-	void testTel(){
-		for (int i = 0; i < 10; i++) {
-			System.out.print(makeTel() + " ");
-		}
-	}
+//	@Test
+//	void testTel(){
+//		for (int i = 0; i < 10; i++) {
+//			System.out.print(makeTel() + " ");
+//		}
+//	}
 
 	@Test
-	void testID(){
+	void testID() {
 		for (int i = 0; i < 10; i++) {
-			System.out.println(makeIDNum(1) );
+			System.out.println(makeIDNum(1));
 		}
 	}
-
-
 
 
 }
